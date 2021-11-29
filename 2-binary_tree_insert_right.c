5d499c9852ee4e75cae4a61e91d953314a31618d
root <3290@holbertonschool.com>
2021-11-29 12:19:03 -0800

    2a prueba

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+/**
+ *
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *rightnew, *auxiliar;
+	rightnew = malloc(sizeof(binary_tree_t));
+	if (!parent)
+		return (NULL);
+
+	rightnew->n = value
+	rightnew->parent = parent;
+	rightnew->left = NULL;
+	
+	if(parent->right == NULL)
+	{
+		rightnew->right = NULL;
+		parent->right = rightnew;
+		return(rightnew);
+	}
+	auxiliar = parent->right;
+	parent->right = rightnew;
+	rightnew->right = auxiliar;
+	return(auxiliar);
+}
