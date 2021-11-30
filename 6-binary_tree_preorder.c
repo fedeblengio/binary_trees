c1c6f74df819a039d192987a769f7c181114f019
root <3290@holbertonschool.com>
2021-11-30 07:59:41 -0800

    fin6

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+#include <stdio.h>
+/**
+ *binary_tree_preorder - function that goes through in pre-order traversal
+ *@tree: tree pointer to the root tree
+ *@func: function for call each node
+ *Return: function
+ */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree && !func)
+		return;
+
+	func(tree->n);
+	if (tree->left != NULL)
+		binary_tree_preorder(tree->left, func);
+	if (tree->right != NULL)
+		binary_tree_preorder(tree->right, func);
+}
