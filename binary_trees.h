fb046531420ff4fc3fc3f085a49e7751169dc4ec
root <3290@holbertonschool.com>
2021-12-01 10:21:10 -0800

    up binary

--- binary_trees.h
+++ binary_trees.h
@@ -39,4 +39,5 @@ size_t binary_tree_depth(const binary_tree_t *tree);
 size_t binary_tree_leaves(const binary_tree_t *tree);
 int binary_tree_balance(const binary_tree_t *tree);
 size_t binary_tree_height(const binary_tree_t *tree);
+int binary_tree_is_perfect(const binary_tree_t *tree);
 #endif
