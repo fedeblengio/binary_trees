4acd608570144d36a57e92df44572dc9a967b305
root <3290@holbertonschool.com>
2021-12-01 05:41:15 -0800

    fix

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -7,12 +7,10 @@
  */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (!tree && !func)
-		return;
-
-	if (tree->left != NULL)
-		binary_tree_postorder(tree->left, func);
-	if (tree->right != NULL)
-		binary_tree_postorder(tree->right, func);
-	func(tree->n);
+	if (tree != NULL && func != NULL)
+		if (tree->left != NULL)
+			binary_tree_postorder(tree->left, func);
+		if (tree->right != NULL)
+			binary_tree_postorder(tree->right, func);
+		func(tree->n);
 }
