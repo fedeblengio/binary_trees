c6dd2e0a985690d97a22d9a10a674fbc5df02a53
Federico Blengio <fedeeblengio@gmail.com>
2021-11-30 23:30:50 -0800

    task-1

--- binary_trees.h
+++ binary_trees.h
@@ -29,5 +29,5 @@ void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 int binary_tree_is_leaf(const binary_tree_t *node);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
-
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 #endif
