538df6231e0c0fc53d5ae7403b01b5bb486be9b5
Federico Blengio <fedeeblengio@gmail.com>
2021-12-02 10:36:43 -0800

    fix 13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,10 +1,8 @@
 #include "binary_trees.h"
-
 /**
- * binary_tree_nodes - counts the nodes with at least 1 child in a binary tree
- * @tree: pointer to the root node of the tree to count the number of nodes
- * ------------------------------------------
- * Return: count of nodes
+ * binary_tree_nodes - counts the nodes
+ * @tree: pointer to the root
+ * Return: nodes
 */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
