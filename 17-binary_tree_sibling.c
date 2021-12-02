2ead94920515da87deb5d569d9150839e62718cc
Federico Blengio <fedeeblengio@gmail.com>
2021-12-02 11:10:35 -0800

    rewrite task nice chequer

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -1,15 +1,24 @@
 #include "binary_trees.h"
 /**
- * binary_tree_sibling - finds the sibling
- * @node: pointer node
- * Return: sibling node
- * @node: returns NULL
+ * binary_tree_sibling - find sibling
+ * @node: node
+ * Return: return NULL
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-if (!node->parent || !node)
+
+binary_tree_t *izq;
+binary_tree_t *der;
+
+if (!node)
 return (NULL);
-if ((node->parent->right) == node)
-return (node->parent->left);
-return (node->parent->right);
+if (!node->parent)
+return (NULL);
+
+der = node->parent->right;
+izq = node->parent->left;
+
+if (der == node)
+return (izq);
+return (der);
 }
