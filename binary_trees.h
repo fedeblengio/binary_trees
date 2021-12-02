04b693b42025f422abe1de1cec7cd5ea09618385
Federico Blengio <fedeeblengio@gmail.com>
2021-12-02 02:01:21 -0800

    update

--- binary_trees.h
+++ binary_trees.h
@@ -42,4 +42,5 @@ size_t binary_tree_height(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
+size_t binary_tree_size(const binary_tree_t *tree);
 #endif
